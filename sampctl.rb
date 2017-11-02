a3679abdeb64a5c6fa4f3faa392edf91af90d0ea
goreleaserbot <goreleaser@carlosbecker.com>
2017-11-02 15:03:34 +0000

    sampctl version 1.3.0-RC1

--- sampctl.rb
+++ sampctl.rb
@@ -1,9 +1,9 @@
 class Sampctl < Formula
   desc "A small utility for starting and managing SA:MP servers with better settings handling and crash resiliency."
   homepage "https://github.com/Southclaws/sampctl"
-  url "https://github.com/Southclaws/sampctl/releases/download/1.2.7-R2/sampctl_1.2.7-R2_darwin_amd64.tar.gz"
-  version "1.2.7-R2"
-  sha256 "31caa79c7841bae454c473bfbfaedc0e77a395c5686a2b53f2a9d316de8159e0"
+  url "https://github.com/Southclaws/sampctl/releases/download/1.3.0-RC1/sampctl_1.3.0-RC1_darwin_amd64.tar.gz"
+  version "1.3.0-RC1"
+  sha256 "e96768601e8fdd5ac10fe7088bd07ad9e325be0d322c3bdd025076c36dcf1ffb"
 
   def install
     bin.install "sampctl"
