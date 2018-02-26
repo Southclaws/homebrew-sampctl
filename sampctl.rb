d9116a5c732743962fd7e829bba31a1b509b366a
goreleaserbot <goreleaser@carlosbecker.com>
2018-02-26 00:31:04 +0000

    sampctl version 1.6.11

--- sampctl.rb
+++ sampctl.rb
@@ -1,9 +1,9 @@
 class Sampctl < Formula
   desc "The Swiss Army Knife of SA:MP - vital tools for any server owner or library maintainer."
   homepage "https://github.com/Southclaws/sampctl"
-  url "https://github.com/Southclaws/sampctl/releases/download/1.6.10/sampctl_1.6.10_darwin_amd64.tar.gz"
-  version "1.6.10"
-  sha256 "c2c66c5b4ee25030a3bab1bcc20e0b70f1183e99bbd1b99ff493fb6c33c057ab"
+  url "https://github.com/Southclaws/sampctl/releases/download/1.6.11/sampctl_1.6.11_darwin_amd64.tar.gz"
+  version "1.6.11"
+  sha256 "9dd1eb862541f89397ba468901cb93af2d18658ccac55a1fba46ec8dc2e4b830"
 
   def install
     bin.install "sampctl"
