4e6d02756b7fe0c197933040b2d4b9d26ab144b4
goreleaserbot <goreleaser@carlosbecker.com>
2018-07-07 22:56:20 +0100

    Brew formula update for sampctl version 1.8.14

--- sampctl.rb
+++ sampctl.rb
@@ -1,9 +1,9 @@
 class Sampctl < Formula
   desc "The Swiss Army Knife of SA:MP - vital tools for any server owner or library maintainer."
   homepage "https://github.com/Southclaws/sampctl"
-  url "https://github.com/Southclaws/sampctl/releases/download/1.8.13/sampctl_1.8.13_darwin_amd64.tar.gz"
-  version "1.8.13"
-  sha256 "5a3e8bff067d66d3f1ec09b96881fa71880eace8f3ecc6d0f6a9c84f849f4ce8"
+  url "https://github.com/Southclaws/sampctl/releases/download/1.8.14/sampctl_1.8.14_darwin_amd64.tar.gz"
+  version "1.8.14"
+  sha256 "1c52f47b9f1524140c29968e9feead2c111b2068c7bfdac98ce5e3e47d8ba462"
 
   def install
     bin.install "sampctl"
